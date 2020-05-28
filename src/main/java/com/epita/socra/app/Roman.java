bb0dee20c88825bbc38a9506073890c268754339
Youssef Benkirane <youssef.benkirane@epita.fr>
2020-05-28 16:53:06 +0200

    [Roman] Implement toRoman function : take int return decimal.

---
+++ src/main/java/com/epita/socra/app/Roman.java
@@ -0,0 +1,15 @@
+package com.epita.socra.app;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Roman {
+    public String toRoman(int decimal)
+    {
+        var unit = new ArrayList<String>(Arrays.asList("", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"));
+        var dec = new ArrayList<String>(Arrays.asList("", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"));
+        var cent = new ArrayList<String>(Arrays.asList("", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"));
+        var mill = new ArrayList<String>(Arrays.asList("", "M", "MM", "MMM"));
+        return mill.get(decimal / 1000) + cent.get((decimal / 100) % 10) + dec.get((decimal / 10) % 10) + unit.get(decimal % 10);
+    }
+}
